e502fbd37f50e0d39dbf5bd525b4698faaec040e
Hironori Ogawa <hironori.ogawa@groove-x.com>
2020-05-10 11:58:26 +0900

    use tf2

--- nav_lecture/src/costmap_test.cpp
+++ nav_lecture/src/costmap_test.cpp
@@ -1,10 +1,11 @@
 #include <ros/ros.h>
-#include <tf/transform_listener.h>
+#include <tf2_ros/buffer.h>
 #include <costmap_2d/costmap_2d_ros.h>
 
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "costmap");
-  tf::TransformListener tf(ros::Duration(10));
+  tf2_ros::Buffer tf;
   costmap_2d::Costmap2DROS costmap("my_costmap", tf);
   ros::NodeHandle n;
   costmap.start();
